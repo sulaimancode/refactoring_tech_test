a014c7d6bcee355e2709bc4286cbc9cfade23bbe
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-06-15 13:34:25 +0100

    Exctracts upgrading, degrading and setting quality to 0 to private methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,54 +1,58 @@
 class GildedRose
 
+  SPECIAL_ITEMS = ["Aged Brie",
+                   "Backstage passes to a TAFKAL80ETC concert",
+                   "Sulfuras, Hand of Ragnaros"]
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+      @items.each do |item|
+        next if item.name == SPECIAL_ITEMS[2]
+        unless SPECIAL_ITEMS.include? item.name
+          degrade_quality_of(item)
         else
           if item.quality < 50
             item.quality = item.quality + 1
+            if item.name == SPECIAL_ITEMS[1]
+              if item.sell_in < 11
+                upgrade_quality_of(item)
+              end
+              if item.sell_in < 6
+                upgrade_quality_of(item)
+              end
+            end
+          end
+        end
+          item.sell_in = item.sell_in - 1
+        if item.sell_in < 0
+          unless item.name == SPECIAL_ITEMS[0]
+            unless item.name == SPECIAL_ITEMS[1]
+              degrade_quality_of(item)
+            else
+              set_quality_to_zero_of(item)
+            end
+          else
+            upgrade_quality_of(item)
           end
         end
       end
     end
-  end
+
+private
+
+    def set_quality_to_zero_of(item)
+      item.quality -= item.quality
+    end
+
+    def degrade_quality_of(item)
+      item.quality -= 1 if (item.quality > 0)
+    end
+
+    def upgrade_quality_of(item)
+      item.quality += 1 if (item.quality < 50)
+    end
+
 end
